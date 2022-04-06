a7631eb2600f75ee4ab06376ef861125abee393d
Saielay09 <estefanycielyecgf@gmail.com>
2022-04-06 17:49:22 -0300

    segundo commit

---
+++ jogodavelha.js
@@ -0,0 +1,17 @@
+var jogodavelha={
+    board:["","","","","","","","",""],
+    simbols:["X","0"],
+    container_element: null,
+
+    init:function(container){
+        this.container_element=container;
+    },
+
+    draw: function(){
+        let content = '';
+        for (i in thid.board){
+            content += '>'
+        }
+
+    }
+}
